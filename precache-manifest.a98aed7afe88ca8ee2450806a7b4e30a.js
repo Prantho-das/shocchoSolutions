5fafe804a760625814eaa249d397c3aa830ad74d
prantho kumar <pranthokumardas786@gmail.com>
2020-11-13 18:34:38 +0600

    Updates

---
+++ precache-manifest.a98aed7afe88ca8ee2450806a7b4e30a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8d59f6be584300abfe9104743b412d",
+    "url": "/shocchoSolutions/index.html"
+  },
+  {
+    "revision": "1875b12388aab58f5c2e",
+    "url": "/shocchoSolutions/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "f571c00d11b71fc49f10",
+    "url": "/shocchoSolutions/static/css/main.53602be5.chunk.css"
+  },
+  {
+    "revision": "1875b12388aab58f5c2e",
+    "url": "/shocchoSolutions/static/js/2.11035a6c.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/shocchoSolutions/static/js/2.11035a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f571c00d11b71fc49f10",
+    "url": "/shocchoSolutions/static/js/main.24e0869e.chunk.js"
+  },
+  {
+    "revision": "a1b88c0d06f4769f9a3f",
+    "url": "/shocchoSolutions/static/js/runtime-main.f9f6e045.js"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/shocchoSolutions/static/media/hero.e981619f.svg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/shocchoSolutions/static/media/hero1.619f0cc8.png"
+  }
+]);
